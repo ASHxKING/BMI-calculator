91fc3103d37067db402b4ad8d6f5b2aa0a0d9bbb
ash <getseth00@gmail.com>
2024-04-26 23:35:38 +0530

    added ejs template

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import express from "express"
-import bodyParser from "body-parser"
-import { fileURLToPath } from 'url';
-import path from 'path';
+import express from "express";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const app = express();
 
@@ -9,35 +9,43 @@ const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // middleware for body-parser
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req,res){
-  res.sendFile(__dirname + "/public/bmiCalculator.html")
-})
+app.get("/", function (req, res) {
+  let options = {
+    bmivalue: "",
+    weightvalue: "",
+    heightvalue: "",
+    bmitext: "",
+  };
+  res.render("index.ejs", options);
+});
 
-app.post("/" , function(req,res){
-
+app.post("/", function (req, res) {
   var weight = Number(req.body.weight);
   var height = Number(req.body.height);
 
-  var BMI = weight/(height*height);
+  var BMI = weight / (height * height);
+  let options = {
+    bmivalue: BMI,
+    weightvalue: weight,
+    heightvalue: height,
+  };
 
-  if (BMI <= 18.4){
-    res.send("You are Underweight and your BMI is "+BMI)
+  if (BMI <= 18.4) {
+    options.bmitext = "You are Underweight ";
+  } else if (BMI > 18.4 && BMI <= 24.9) {
+    options.bmitext = "You are Normal ";
+  } else if (BMI > 24.9 && BMI <= 39.9) {
+    options.bmitext = "You are Overweight ";
+  } else if (BMI > 39.5) {
+    options.bmitext = "You are Obese";
   }
-  else if (BMI > 18.4 && BMI <= 24.9) {
-    res.send("You are Normal and your BMI is "+BMI)
-  }
-  else if (BMI > 24.9 && BMI <= 39.9) {
-    res.send("You are Overweight and your BMI is "+BMI)
-  }
-  else if (BMI >39.5) {
-    res.send("You are Obese and your BMI is "+BMI)
-  }
-})
-app.listen(3000,function(){
-  console.log("server is running on port 3000")
-})
+  res.render("index.ejs", options);
+});
+app.listen(3000, function () {
+  console.log("server is running on port 3000");
+});
